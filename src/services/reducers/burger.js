188f419c70a8d0d96db31d70a55f4ec904ef6cbf
Ilya Nikishin <inikishin@gmail.com>
2021-07-05 16:14:59 +0300

    Обновление итога при удалении

--- src/services/reducers/burger.js
+++ src/services/reducers/burger.js
@@ -51,23 +51,23 @@ export const burger = (state = initialState, action) => {
         }
 
         case ADD_INGREDIENT_TO_BURGER: {
-            let ingredient = state.ingredients.filter(element => element._id === action.ingredientId.id);
+            let ingredient = state.ingredients.find(element => element._id === action.ingredientId.id);
             let newState = {};
 
-            if (ingredient[0].type === "bun") {
+            if (ingredient.type === "bun") {
                 newState = {...state,
-                currentBurger: {...state.currentBurger, bun: ingredient[0]},
-                ingredients: [...state.ingredients.filter(item => item._id !== ingredient[0]._id),
-                    {...state.ingredients.filter(item => item._id === ingredient[0]._id)[0], counter: 1}]}
+                currentBurger: {...state.currentBurger, bun: ingredient},
+                ingredients: [...state.ingredients.filter(item => item._id !== ingredient._id),
+                    {...state.ingredients.filter(item => item._id === ingredient._id)[0], counter: 1}]}
             }
             else
             {
-                let item = state.ingredients.filter(item => item._id === ingredient[0]._id)[0];
+                let item = state.ingredients.filter(item => item._id === ingredient._id)[0];
                 item.counter ? item = {...item, counter: item.counter + 1} : item = {...item, counter: 1}
 
                 newState = {...state,
-                    currentBurger: {...state.currentBurger, main: [...state.currentBurger.main, ...ingredient]},
-                    ingredients: [...state.ingredients.filter(item => item._id !== ingredient[0]._id), item]
+                    currentBurger: {...state.currentBurger, main: [...state.currentBurger.main, ingredient]},
+                    ingredients: [...state.ingredients.filter(item => item._id !== ingredient._id), item]
                 }
             }
 
@@ -81,7 +81,12 @@ export const burger = (state = initialState, action) => {
         case DELETE_INGREDIENT_FROM_BURGER: {
             let newMain = state.currentBurger.main;
             newMain.splice(action.ingredientIndex, 1);
-            return {...state, currentBurger: {...state.currentBurger, main: newMain}}
+
+            let total = 0;
+            state.currentBurger.bun.price && (total = state.currentBurger.bun.price * 2);
+            newMain.forEach((item) => {total += item.price});
+
+            return {...state, currentBurger: {...state.currentBurger, main: newMain, total: total}}
         }
 
         default: {
