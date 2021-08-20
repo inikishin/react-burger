61031b870dacffaf38f6c4011e98dea5af0ae589
INikishin <inikishin@gmail.com>
2021-08-20 12:54:24 +0300

    middleware для websocket переведена на typescript

--- src/services/middleware/socketMiddleware.ts
+++ src/services/middleware/socketMiddleware.ts
@@ -1,15 +1,15 @@
 import {WS_CONNECTION_START, WS_CONNECTION_SUCCESS, WS_CONNECTION_ERROR, WS_GET_MESSAGE, WS_SEND_MESSAGE, WS_CONNECTION_CLOSED} from "../actions/feed";
 
 export const socketMiddleware = () => {
-    return store => {
-        let socket = null;
-        return next => action => {
+    return (store: { dispatch: any; }) => {
+        let socket: WebSocket | null = null;
+        return (next: (arg0: any) => void) => (action: { type: any; payload: any; }) => {
             const {dispatch} = store;
             const {type, payload} = action;
 
             if (type === WS_CONNECTION_START) {
                 if (socket) {
-                    socket.close('1000', 'reconnect needed');
+                    socket.close(1000, 'reconnect needed');
                 }
                 if (!payload) {
                     socket = new WebSocket('wss://norma.nomoreparties.space/orders/all');
